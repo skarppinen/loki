fa2b3de54e305a61bbb7cb239d93e81098823793
Santeri Karppinen <santeri.karppinen@fmi.fi>
2023-09-19 08:53:15 +0300

    new branch stable for tracking from other repos

--- transformations/transformations/single_column_coalesced.py
+++ transformations/transformations/single_column_coalesced.py
@@ -5,7 +5,7 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-import re
+import re 
 from loki.expression import symbols as sym
 from loki.transform import resolve_associates, inline_member_procedures
 from loki import (
